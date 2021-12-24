3050d6d20843de2402ba237b3ffed3e47f5eda3a
JoakimGit <jokke34@gmail.com>
2021-12-24 15:30:25 +0100

    Created basic sidebar-header-content layout.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
+import { ProjectModule } from './project/project.module';
+import { TicketModule } from './ticket/ticket.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path:'ticket',
+    loadChildren: () => import('./ticket/ticket.module').then(m => m.TicketModule)
+  },
+  {
+    path:'project',
+    loadChildren: () => import('./project/project.module').then(m => m.ProjectModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/project/project-routing.module.ts
+++ frontend/src/app/project/project-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectComponent } from './components/project-list/project.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProjectComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
